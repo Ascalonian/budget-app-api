c8b43962cef6bafad28fd14059150d5355ad40fd
Michael Judd <judd.michael@gmail.com>
2020-06-14 03:34:27 -0400

    Initial checkin - example of how to redirect Tomcat to https

---
+++ src/main/java/com/majicode/budgetapp/configuration/WebConfig.java
@@ -0,0 +1,52 @@
+package com.majicode.budgetapp.configuration;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * This class will redirect all traffic from http://localhost:8081 to https://localhost:8443
+ * Need to change the port defined in the application.yml to 8443 to work
+ * 
+ * @author MikeyJudd
+ *
+ */
+
+//@Configuration
+public class WebConfig {
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		final TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+			@Override
+			protected void postProcessContext(Context context) {
+				final SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+				final SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		final Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8081);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+
+		return connector;
+	}
+}
