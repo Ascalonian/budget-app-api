cba32da3ea2fb687aacfaa21ee161b2dc69b609e
Michael Judd <judd.michael@gmail.com>
2020-06-21 20:32:37 -0400

    Removed the /name endpoint

--- src/main/java/com/majicode/budgetapp/BudgetAppApiSpringBoot.java
+++ src/main/java/com/majicode/budgetapp/BudgetAppApiSpringBoot.java
@@ -1,17 +1,10 @@
 package com.majicode.budgetapp;
 
-import org.openapitools.jackson.nullable.JsonNullableModule;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.fasterxml.jackson.databind.Module;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,10 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @ComponentScan(basePackages = {"com.majicode.budgetapp", "com.majicode.budgetapp.api" , "com.majicode.budgetapp.configuration"})
 public class BudgetAppApiSpringBoot implements CommandLineRunner {
-	
-	@Value("${spring.application.name}")
-	private String name;
-	
 	@Override
     public void run(String... arg0) throws Exception {
         if (arg0.length > 0 && arg0[0].equals("exitcode")) {
@@ -42,29 +31,6 @@ public class BudgetAppApiSpringBoot implements CommandLineRunner {
             return 10;
         }
 
-    }
-    
-    @RequestMapping(value = "/name")
-    public String name() {
-       return name;
-    }
-
-    @Bean
-    public WebMvcConfigurer webConfigurer() {
-        return new WebMvcConfigurer() {
-            /*@Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("Content-Type");
-            }*/
-        };
-    }
-
-    @Bean
-    public Module jsonNullableModule() {
-        return new JsonNullableModule();
     }
 
 }
