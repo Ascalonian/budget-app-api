bbe123be685ddaba9bbd782b28f0898799b304cb
Mikey Judd <judd.michael@gmail.com>
2020-08-02 02:41:10 -0400

    Just updated styling

--- src/main/java/com/majicode/budgetapp/BudgetAppApiSpringBoot.java
+++ src/main/java/com/majicode/budgetapp/BudgetAppApiSpringBoot.java
@@ -5,16 +5,15 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = {"com.majicode.budgetapp", "com.majicode.budgetapp.api" , "com.majicode.budgetapp.configuration"})
+@ComponentScan(basePackages = {"com.majicode.budgetapp", "com.majicode.budgetapp.api", "com.majicode.budgetapp.configuration"})
 public class BudgetAppApiSpringBoot implements CommandLineRunner {
 	@Override
     public void run(String... arg0) throws Exception {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+		if (arg0.length > 0 && arg0[0].equals("exitcode")) {
             throw new ExitException();
         }
     }
